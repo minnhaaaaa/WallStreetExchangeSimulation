988c12a3434558e2863e568e7b0e473a79e18c40
Minha Shameer <179120250+minnhaaaaa@users.noreply.github.com>
2025-11-01 07:54:57 +0530

    Add Stock and Market classes for stock management

--- market.py
+++ market.py
@@ -1,1 +1,14 @@
+class Stock:
+    def __init__(self, name, symbol, price, volatility):
+        self.name = name
+        self.symbol = symbol
+        self.price = price
+        self.volatility = volatility  
+
+class Market:
+    def __init__(self):
+        self.stocks = {
+            "AAPL": Stock("Apple Inc.", "AAPL","Technology", 120.0, 0.03),
+            "XOM": Stock("Exxon Mobil", "XOM","Energy", 55.0, 0.04),
+        }
 
