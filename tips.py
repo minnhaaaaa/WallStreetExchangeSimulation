7fae4deefe7599276dd1e98a3596819f71ec354d
Minha Shameer <179120250+minnhaaaaa@users.noreply.github.com>
2025-11-03 20:05:00 +0530

    Fix formatting and typos in tips.py
    Corrected bugs

--- tips.py
+++ tips.py
@@ -9,28 +9,27 @@ def generate_tip(stocks):
     tips_positive = [
         f"Word is out that {name} is about to announce record profits!",
         f"Rumor: {name} may merge with a top tech company.",
-        f"Sources say {name} just secured a massive government contract."
-        f"Early reports suggest {name} will exceed quarterly expectations."
-        f"{name}set to enter new market with strong growth potential."
-        f"Investor sentiment for{name} is at an all-time high."
-        f"Insiders cliam {name} is in talks for international expansion."
-        f"{name} is rumored to announce a breakthrough product next quarter."
-        f"Speculation mounts that{name} will issue a dividend increase."
-        f"{name}'s CEO scheduled for a major keynote at an  industry conference
+        f"Sources say {name} just secured a massive government contract.",
+        f"Early reports suggest {name} will exceed quarterly expectations.",
+        f"{name} set to enter new market with strong growth potential.",
+        f"Investor sentiment for {name} is at an all-time high.",
+        f"Insiders claim {name} is in talks for international expansion.",
+        f"{name} is rumored to announce a breakthrough product next quarter.",
+        f"Speculation mounts that {name} will issue a dividend increase.",
+        f"{name}'s CEO scheduled for a major keynote at an industry conference",
     ]
 
     tips_negative = [
         f"Leaked memo: {name} might be under investigation.",
         f"Analysts suspect {name} is overvalued — expect a dip.",
-        f"Rumor says {name} is facing internal layoffs soon."
-        f"Whistleblower alleges accounting irregularities at {name}."
-        f"supply chain disruptions may impact {name}'s earnings."
-        f"{name} faces stiff competition in a shrinking market."
-        f"Pending lawsuit could affect {name}'s leadership."
-        f"Unconfirmed reports of customer data breach at (name}."
-        f"Global market instability may hit{name}'s overseas revenue."
-        f"Pending lawsuit could affect {name}'s financial stability."
-        
+        f"Rumor says {name} is facing internal layoffs soon.",
+        f"Whistleblower alleges accounting irregularities at {name}.",
+        f"supply chain disruptions may impact {name}'s earnings.",
+        f"{name} faces stiff competition in a shrinking market.",
+        f"Pending lawsuit could affect {name}'s leadership.",
+        f"Unconfirmed reports of customer data breach at {name}.",
+        f"Global market instability may hit {name}'s overseas revenue.",
+        f"Pending lawsuit could affect {name}'s financial stability.",   
     ]
     if direction == "up":
         message = random.choice(tips_positive)
